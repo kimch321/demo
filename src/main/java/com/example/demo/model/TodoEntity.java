601f3b3d14a51e60a0b9b96e6ae48ee3f327d765
ChungIl <cnddlf12@naver.com>
2023-08-12 14:42:26 +0900

    4.3 Spring Security

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -19,11 +19,10 @@ import javax.persistence.Table;
 @Table(name = "Todo")
 public class TodoEntity {
     @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    private String id;  // 이 오브젝트의 아이디
-    private String userId;  // 이 오브젝트를 생성한 사용자의 아이디
-    private String title;  // Todo 타이틀(예: 운동하기)
-    private boolean done;  // true - todo를 완료한 경우(checked)
-
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id; // 이 오브젝트의 아이디
+    private String userId; // 이 오브젝트를 생성한 유저의 아이디
+    private String title; // Todo 타이틀 예) 운동 하기
+    private boolean done; // true - todo를 완료한 경우(checked)
 }
--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,11 +2,16 @@ package com.example.demo.persistence;
 
 import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TodoRepositoty extends JpaRepository<TodoEntity, String> {
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
     List<TodoEntity> findByUserId(String userId);
+
+    @Query("SELECT t FROM TodoEntity t WHERE t.userId = ?1")
+    TodoEntity findByUserIdQuery(String userId);
+
 }
